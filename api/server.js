d9ad9285860a77dc8cd6b433e93b457564808a5c
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-22 14:53:05 -0600

    sets up error handling in server

--- api/server.js
+++ api/server.js
@@ -8,4 +8,11 @@ server.get('/', (req, res) => {
     res.json({message: "successfully wired"})
 })
 
+server.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message
+    })
+})
+
 module.exports = server
